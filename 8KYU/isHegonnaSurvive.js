fd97c2e91037d70677c59bc776ab1252740022d1
igorntwari <igorntwari28@gmail.com>
2023-08-12 17:38:22 +0200

    added new kata

---
+++ 8KYU/isHegonnaSurvive.js
@@ -0,0 +1,8 @@
+function hero(bullets, dragons){
+    //Get Coding!
+      // calculate the total bullets needed to defeat the dragons 
+      // return the bullets which are higher or equal to the calculated bullets 
+      
+      const bulletsToDefeat = dragons * 2;
+      return bullets >= bulletsToDefeat;
+    }
